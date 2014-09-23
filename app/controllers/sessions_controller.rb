d2f23e3b922d1b660804d5f9b4729d2d2cd5d13c
Andrew McNutt <mcnutt.andrew@gmail.com>
2014-09-23 14:50:21 -0700

    api done

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,12 @@
 class SessionsController < ApplicationController
   
   def new
+    redirect_to user_feed_url unless logged_in?
     render :new
   end
   
   def create
+    redirect_to user_feed_url unless logged_in?
     user = User.find_by_credentials(
       params[:user][:username],
       params[:user][:password]
@@ -12,7 +14,7 @@ class SessionsController < ApplicationController
     
     if user
       login(user)
-      redirect_to user_url(user)
+      redirect_to user_feed_url
     else
       flash.now[:errors] = ["Invalid Username/Password Combination"]
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   
   resources :comments, except: [:create, :edit, :update]
   
-  #custom route for feed
+  
 end

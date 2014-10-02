768868cff8264748c642f68d8a30dee23bffa86d
Andrew McNutt <mcnutt.andrew@gmail.com>
2014-10-02 16:30:37 -0700

    Adds dynamic searching

--- app/assets/javascripts/views/search.js
+++ app/assets/javascripts/views/search.js
@@ -6,31 +6,45 @@ Teacup.Views.searchUsers = Backbone.View.extend({
 	},
 	
 	events: {
-		"submit .userSearch": "submit"
+		"submit .userSearch": "submit",
+		"keydown .userSearch": "keypresser"
 	},
 	
 	render: function(){
 		var that = this;
 		var renderedContent = that.template({
-			users: this.users
+			users: this.users,
+			search: this.searchterm
 		});
 		that.$el.html(renderedContent);
 		return that;
 	},
 	
-	submit: function(event){
-		event.preventDefault();
+	keypresser: function(event){
+		var that = this
 		var formData = $(event.currentTarget).serializeJSON();
-		var that = this;
+		console.log(formData["query"]);
 		$.ajax({
-			url: ('api/users/search'),
+			url: 'api/users/search',
 			type: 'GET',
 			data: {query: formData["query"]},
 			success: function(users){
-				that.users = users;
-				that.render()
+				var userList = that.$el.find(".users-list");
+				userList.empty();
+				for(var i= 0; i < users.length; i++){
+					var listAppend = "<li class='searchCell'><a href='#/users/" + users[i].id+ "'>"
+					listAppend = listAppend + "<h3> " + users[i].username+ " </h3></a></li>"
+					userList.append(listAppend);
+				}
+				
 			}
 		});
+		
+		
+	},
+	
+	submit: function(event){
+		event.preventDefault();
 	}
 	
 })
